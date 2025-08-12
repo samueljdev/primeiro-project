f3ad7736bcc34c5c59a108432b99c46290518e3f
samueljdev <samuel.jdev11@gmail.com>
2025-08-12 00:25:57 -0400

    Criando primeiros componentes

---
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meu-primeiro',
+  template: `<p>Meu primeiro component com Angular 2!</p>`,
+})
+export class MeuPrimeiroComponent { }
